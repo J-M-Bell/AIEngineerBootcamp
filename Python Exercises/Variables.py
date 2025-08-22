715f761a56c16f5263b9e5802bfba2d9cf6f7797
JM Bell <justinmbell1997@gmail.com>
2025-08-21 20:49:20 -0500

    variable python exercise completed

---
+++ Python Exercises/Variables.py
@@ -0,0 +1,42 @@
+'''
+Exercise #1
+Create a variable with name x and a value of 10. Execute.
+Tell the computer to show you the value of that variable without utilizing the print() function, similar to what you would do in Jupyter.
+'''
+
+x = 10
+x
+
+'''
+Exercise #2
+Create a variable with name x and a value of 10. Execute
+Can you think of a second way to obtain the same result?
+'''
+
+x = 10
+print(x)
+
+
+'''
+Exercise #3
+On the same line, create four new variables: a, b, c, and d, that are equal to 10, 20, 30, and 40, respectively.
+'''
+
+a,b,c,d = (10,20,30,40)
+
+
+'''
+Exercise #4
+On the same line, create four new variables: a, b, c, and d, that are equal to 10, 20, 30, and 40, respectively.
+Tell the computer to show you the value corresponding to the variable b.
+Do the same for d.
+'''
+
+a,b,c,d = (10,20,30,40)
+print(b)
+print(d)
+
+
+'''
+
+'''

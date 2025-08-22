35afa2f57068fea7163f493ca05bc2abe0a539aa
JM Bell <justinmbell1997@gmail.com>
2025-08-22 16:19:13 -0500

    complete data type exercises

---
+++ Python Exercises/DataTypes.py
@@ -0,0 +1,45 @@
+'''
+Exercise #1
+Create a variable a equal to "True".
+Check its type.
+'''
+
+a = True
+type(a)
+
+
+'''
+Exercise #2
+Create a variable b equal to 99.
+Check its type.
+'''
+
+b = 99
+type(b)
+
+
+'''
+Exercise #3
+Check the type of the value 0.99.
+'''
+
+type(0.99)
+
+
+'''
+Exercise #4
+Create a variable b equal to 99.
+Turn b into a float.
+Consider using the print() function as a tool to help you solve this exercise.
+'''
+
+b = 99
+b = float(b)
+print(b)
+
+'''
+Exercise #5
+Turn 0.99 into an integer.
+'''
+
+int(0.99)

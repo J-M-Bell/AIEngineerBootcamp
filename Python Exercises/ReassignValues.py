edcc320fef21db98a71fc5760cbe4febaeea3b6d
JM Bell <justinmbell1997@gmail.com>
2025-08-24 12:19:44 -0500

    completed reassign variables exercises

---
+++ Python Exercises/ReassignValues.py
@@ -0,0 +1,38 @@
+'''
+Exercise #1
+Assign the value of 14 to a variable p.
+'''
+
+p = 14
+
+
+'''
+Exercise #2
+Assign the value of 14 to a variable p.
+Calculate p + 10.
+'''
+
+p = 14
+p + 10
+
+
+'''
+Exercise #3
+Now, assign 30 to the variable p.
+'''
+
+p = 30
+
+
+'''
+Exercise #4
+Calculate p + 10.
+'''
+
+p = 30
+p + 10
+
+
+'''
+
+'''

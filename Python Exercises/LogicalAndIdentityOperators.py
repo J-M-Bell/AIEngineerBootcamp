85a88b3c80e676c152729559bde5c5ec0a6e1a6a
JM Bell <justinmbell1997@gmail.com>
2025-08-25 20:15:10 -0500

    completed logical and identity operators exercises

---
+++ Python Exercises/LogicalAndIdentityOperators.py
@@ -0,0 +1,50 @@
+'''
+Exercise #1
+Check whether the following code is True or False.
+False or not True and not False
+'''
+
+False or not True and not False
+
+
+'''
+Exercise #2
+Check whether the following code is True or False.
+True and not False and True or not False
+'''
+
+True and not False and True or not False
+
+
+'''
+Exercise #3
+Check whether the following code is True or False.
+True or False and False
+'''
+
+True or False and False
+
+
+'''
+Exercise #4
+Check whether the following code is True or False.
+False and True or False
+'''
+
+False and True or False
+
+
+'''
+Exercise #5
+Using an identity operator, verify that 10 is not the same as 12.
+'''
+
+10 is not 12
+
+
+'''
+Exercise #6
+Using an identity operator, verify that 50 is the same as 50.
+'''
+
+50 is 50

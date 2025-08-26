77cd95f86cecce0229316414d6f8789f1da4cd0b
JM Bell <justinmbell1997@gmail.com>
2025-08-25 20:15:44 -0500

    began lists python exercises

---
+++ Python Exercises/Lists.py
@@ -0,0 +1,44 @@
+'''
+Lists - Exercise #1
+Create a list, called Numbers. Let it contain the numbers 10, 25, 40, and 50.
+Print the element at index 2 from the list.
+'''
+
+Numbers = [10, 25, 40, 50]
+print(Numbers[2])
+
+
+'''
+Exercise #2
+Print the 0th element from the Numbers list.
+'''
+
+Numbers = [10, 25, 40, 50]
+print(Numbers[0])
+
+
+'''
+Exercise #3
+Print the third-to-last element from the Numbers list using a minus sign in the brackets.
+'''
+
+Numbers = [10, 25, 40, 50]
+print(Numbers[-3])
+
+
+'''
+Exercise #4
+Replace the number 10 with the number 15 in the Numbers list.
+'''
+
+Numbers = [10, 25, 40, 50]
+Numbers[0] = 15
+
+
+'''
+Exercise #5
+Delete the number 25 from the Numbers list.
+'''
+
+Numbers = [10, 25, 40, 50]
+del Numbers[1]

43d906d5c5e69896805b0b3653641417cba8689d
JM Bell <justinmbell1997@gmail.com>
2025-08-25 20:13:22 -0500

    completed arithmetic operators exercises

---
+++ Python Exercises/ArithmeticOperators.py
@@ -0,0 +1,64 @@
+'''
+Exercise #1
+Add the integers 15 and 23.
+'''
+
+15 + 23
+
+
+'''
+Exercise #2
+Subtract 50 from 26.
+'''
+
+26 - 50
+
+
+'''
+Exercise #3
+Divide 20 by 4.
+'''
+
+20 / 4
+
+
+'''
+Exercise #4
+Divide 22 by 4.
+'''
+
+22 / 4
+
+
+'''
+Exercise #5
+Obtain the remainder of the division of 22 by 4.
+'''
+
+22 % 4
+
+
+'''
+Exercise #6
+Divide the float 22 by 4.
+'''
+
+float(22) / 4
+
+
+'''
+Exercise #7
+Multiply 6 by 8.
+'''
+
+6 * 8
+
+
+'''
+Exercise #8
+Raise 15 to the power of 2.
+'''
+
+15 ** 2
+
+

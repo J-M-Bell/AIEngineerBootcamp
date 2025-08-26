3939d876e4aaa396d75351e07a7e19a92c7bdb17
JM Bell <justinmbell1997@gmail.com>
2025-08-25 20:13:42 -0500

    completed comparison operators exercises

---
+++ Python Exercises/ComparisonOperators.py
@@ -0,0 +1,30 @@
+'''
+Exercise #1
+Verify that 25 is smaller than 30.
+'''
+
+25 < 30
+
+
+'''
+Exercise #2
+Verify that 5 multiplied by 3 is less than or equal to 5 to the power of 3.
+'''
+
+5 * 3 <= 5 ** 3
+
+
+'''
+Exercise #3
+Verify that 100 is equal to 10 square.
+'''
+
+100 == 10 ** 2
+
+
+'''
+Exercise #4
+Verify that 53 is not equal to 46.
+'''
+
+53 != 46

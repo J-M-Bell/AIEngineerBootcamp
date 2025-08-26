5dda058cde4841e5146276bd91b25585dc961b98
JM Bell <justinmbell1997@gmail.com>
2025-08-25 20:15:26 -0500

    completed strings python exercises

---
+++ Python Exercises/Strings.py
@@ -0,0 +1,41 @@
+'''
+Exercise #1
+Assign the value of 100 to the variable m.
+With the help of the variable m, write one line of code where the output after execution would be 100 days.
+Hint: You could provide four answers to this question!
+'''
+
+m = 100
+print(str(m) + " days")
+
+
+'''
+Exercise #2
+Produce an output equal to It's cool, isn't it? .
+'''
+
+print("It's cool, isn't it?")
+
+
+'''
+Exercise #3
+Fix the string below.
+'Don't be shy
+'''
+
+'Don\'t be shy'
+
+
+'''
+Exercise #4
+Produce an output equal to Click "OK".
+'''
+
+print('Click "OK"')
+
+'''
+Exercise #5
+Include a trailing comma in your line of code to produce Big Houses.
+'''
+
+print("Big", "Houses")

9e6f82285d4bb6ab880a794c0945a83f5b7f4772
JM Bell <justinmbell1997@gmail.com>
2025-08-26 20:59:36 -0500

    completed more list exercises

--- Python Exercises/Lists.py
+++ Python Exercises/Lists.py
@@ -42,3 +42,42 @@ Delete the number 25 from the Numbers list.
 
 Numbers = [10, 25, 40, 50]
 del Numbers[1]
+
+
+'''
+Using Methods - Exercise #1
+Append the number 100 to the Numbers list.
+'''
+
+Numbers = [15, 40, 50]
+Numbers.append(100)
+
+
+'''
+Exercise #2
+With the help of the .extend() method, add the numbers 115 an 140 to the Numbers list.
+'''
+
+Numbers = [15, 40, 50, 100]
+Numbers.extend([115, 140])
+
+
+'''
+Exercise #3
+Print a statement, saying "The fourth element of the Numbers list is" followed by the value of the fourth element of the Numbers list. Ensure you use a trailing comma to concatenate the statement with the value.
+'''
+
+Numbers = [15, 40, 50, 100]
+Numbers.extend([115, 140])
+print("The fourth element of the Numbers list is", Numbers[3])
+
+
+'''
+Exercise #4
+Currently, how many elements are there in the Numbers list?
+'''
+
+
+Numbers = [15, 40, 50, 100]
+Numbers.extend([115, 140])
+print(len(Numbers))

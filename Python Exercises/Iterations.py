e4a95fa9d20794a3ed105461cd6b962ab6cdbf62
JM Bell <justinmbell1997@gmail.com>
2025-08-27 20:43:19 -0500

    completed iterations exercises

---
+++ Python Exercises/Iterations.py
@@ -0,0 +1,43 @@
+'''
+For Loops - Exercise #1
+Create a For loop that prints every digit on a new line.
+'''
+
+digits = [0,1,2,3,4,5,6,7,8,9]
+for n in digits:
+    print(n)
+
+
+'''
+Exercise #2
+Adjust the code, so the digits are all printed on the same line.
+'''
+
+digits = [0,1,2,3,4,5,6,7,8,9]
+
+for d in digits:
+    print (d, end = " ")
+	
+	
+'''
+While Loops and Incrementing - Exercise #1
+Create a while loop that will print all odd numbers from 0 to 30 on the same row.
+'''
+
+x = 1
+while x < 30:
+    print(x, end = " ")
+    x += 2
+	
+
+'''
+Create Lists with the range() Function - Exercise #1
+Use the range() function to create a list with all numbers from 1 to 10.
+'''
+
+list(range(1, 11))
+
+
+'''
+
+'''
